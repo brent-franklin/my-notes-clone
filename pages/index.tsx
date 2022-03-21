ce2f4c4e9ddfb07af1a4ae4b436f77b9fe663591
brent-franklin <brent.j.franklin@gmail.com>
2022-03-21 17:08:19 -0500

    Changed index.tsx getStaticProps and Home function

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,26 +9,28 @@ import NotesApp from '@/components/NotesApp';
 // UtilityBar Context
 import { UtilityProvider } from '../lib/utilityContext';
 
-const Home: NextPage<{ folders: FolderType[]; notes: NoteType[] }> = ({
+const Home: NextPage<{ folders: FolderType[]; notesArr: DBNoteType[] }> = ({
   folders,
-  notes,
+  notesArr,
 }: {
   folders: FolderType[];
-  notes: NoteType[];
-}) => (
-  <div>
-    <Head>
-      <title>My Notes Clone</title>
-      <meta name="description" content="MacOS Notes Clone" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <UtilityProvider>
-      <UtilityBar />
-      <NotesApp folders={[...folders]} notes={[...notes]} />
-    </UtilityProvider>
-  </div>
-);
-
+  notesArr: DBNoteType[];
+}) => {
+  const notes = noteHelper(notesArr);
+  return (
+    <div>
+      <Head>
+        <title>My Notes Clone</title>
+        <meta name="description" content="MacOS Notes Clone" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <UtilityProvider>
+        <UtilityBar />
+        <NotesApp folders={[...folders]} notes={[...notes]} />
+      </UtilityProvider>
+    </div>
+  );
+};
 
 // This function is removed after initially run
 // It returns the initial props for the app so
@@ -40,11 +42,10 @@ export const getServerSideProps: GetServerSideProps = async (
   const folderRes: Response = await fetch(`${process.env.NOTES_URL}api/folders` as string);
   const folders = await folderRes.json().then((folder) => folder.folders);
   const notesRes: Response = await fetch(`${process.env.NOTES_URL}api/notes` as string);
-  const n = await notesRes.json().then((note) => note.notes);
-  const notes = noteHelper(n);
+  const notesArr = await notesRes.json().then((note) => note.notes);
 
   return {
-    props: { folders, notes }, // will be passed to the page component as props
+      props: { folders, notesArr }, // will be passed to the page component as props
   };
 };
 
